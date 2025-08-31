317c39db4f9cf0806e62814f79f219f5d9e602e7
Adebayo Emmanuel <adebayoemmanu1111@gmail.com>
2025-08-31 07:42:09 -0400

    yes

--- course-03-html-css-js/employeeDetails/employee_details.js
+++ course-03-html-css-js/employeeDetails/employee_details.js
@@ -1,8 +1,8 @@
 // Define an array object: Array that contains objects
 const employees = [
-    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    {id: 2, name: 'Alice Smith', age: 28, department: "HR", salary: 45000},
-    {id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000}
+    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Javascript' },
+    {id: 2, name: 'Alice Smith', age: 28, department: "HR", salary: 45000, specialization: 'Python'},
+    {id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java'}
 ];
 
 // We create a function to display all employees --- employees.map(ArrowFunc)
@@ -19,7 +19,7 @@ function calculateTotalSalaries() {
 
 // displayHREmployees
 
-function displayEmployees() {
+function displayHREmployees() {
     const hrEmployees = employees.filter(employee => employee.department === 'HR');
     const hrEmployeesDisplay = hrEmployees.map((employee,index) => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary} </p>`).join('');
     document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
@@ -34,3 +34,14 @@ function findEmployeeById(employeeId) {
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
     }
 }
+
+function findSpecialization(employeeSpecialization) {
+    const foundEmployee = employees.find(employee => employee.specialization === employeeSpecialization);
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - ${foundEmployee.specialization} </p>`;
+    }
+    else {
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this Specialization';
+    }
+}
+
