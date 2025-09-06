9484978598f81306d38bd534dba1959d9cee60df
Adebayo Emmanuel <adebayoemmanu1111@gmail.com>
2025-09-06 13:05:32 -0400

    Update script.js

---
+++ course-03-html-css-js/Simple Interest Calculator/script.js
@@ -0,0 +1,23 @@
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
+
+function compute() {
+    var pricipal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = pricipal * years * rate / 100;
+    var amount = parseInt(pricipal) + parseFloat(interest);
+    var result = document.getElementById("result");
+
+    var year = new Date().getFullYear() + parseInt(years);
+
+    if (pricipal <= 0){
+        alert("Please enter a positive number!");
+        document.getElementById("principal").focus();
+    } else {
+        result.innerHTML = `If you deposit $${pricipal}, \<br\> at an interest rate of of ${rate} %\<br\> You will receive an amount of ${amount}, \<br\> in the year ${year} \<br\>`;
+    }
+}
