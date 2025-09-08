cff83596ec5f593a6a1c1054f156b06d74f9709d
Adebayo Emmanuel <adebayoemmanu1111@gmail.com>
2025-09-07 23:31:41 -0400

    Update script.js

--- course-03-html-css-js/SinglePageWebsite/script.js
+++ course-03-html-css-js/SinglePageWebsite/script.js
@@ -3,7 +3,7 @@ function addRecommendation() {
 
     if (recommendation.value != null && recommendation.value.trim() != "") {
         console.log("New recommendation added");
-
+        showPopup(true);
         var element = document.createElement("div");
         element.setAttribute("class","recommendation");
         element.innerHTML = `\<span\>&#8220;\</span\> ${recommendation.value} \<span\>&#8221;\</span\>`;
@@ -14,4 +14,13 @@ function addRecommendation() {
         // Reset the text area
         recommendation.value = "";
     }
+}
+
+function showPopup(bool) {
+    if (bool) {
+        document.getElementById('popup').style.visibility = 'visible'
+    } else {
+        document.getElementById('popup').style.visibility = 'hidden'
+    }
+
 }
