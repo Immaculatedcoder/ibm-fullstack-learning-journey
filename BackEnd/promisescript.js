25e2770d028c359e44bbcd996539f33caa0df751
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-09-26 00:05:14 -0400

    added some code

--- BackEnd/promisescript.js
+++ BackEnd/promisescript.js
@@ -1,1 +1,19 @@
-var x = 2;
+// Creating a promise method.
+
+let myPromise = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("Promise resolved")
+    },6000)
+})
+
+
+// Console log before calling the promise
+console.log("Before calling the promise");
+
+// Call the promise and wait for it to be resolved and print a message
+myPromise.then(value => {
+    console.log("From Callback " + value)
+})
+
+// Console log after calling the promise
+console.log("After calling the promise");
